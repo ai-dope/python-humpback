56593e3f39dfb39c725128853695d85e3a62c0db
Isaac Springer <onyxhat@users.noreply.github.com>
2025-03-26 08:06:26 -0500

    Create entrypoint.example.sh

---
+++ entrypoint.example.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+python -m venv $VIRTUAL_ENV
+. $VIRTUAL_ENV/bin/activate
+
+echo "[$(python --version)] running @ [$(which python)]"
+
+pip install -q -r requirements.txt 2>&1
+clear
+
+python $@
